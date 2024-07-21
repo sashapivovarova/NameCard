7bb6b0fa31e81c34f24b88628d2756b659e22314
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-07-21 16:30:47 +0900

    Initial Commit

---
+++ NameCard/NameCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameCardApp.swift
+//  NameCard
+//
+//  Created by Anastasia Grey on 2024/07/21.
+//
+
+import SwiftUI
+
+@main
+struct NameCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
