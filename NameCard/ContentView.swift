7d6185912bacb82d7039abe5d877e7a03ae00527
sashapivovarova <78265489+sashapivovarova@users.noreply.github.com>
2024-07-24 20:49:10 +0900

    Image の追加

--- NameCard/ContentView.swift
+++ NameCard/ContentView.swift
@@ -12,10 +12,21 @@ struct ContentView: View {
         ZStack {
             Color(red: 0.09, green: 0.63, blue: 0.52)
                 .edgesIgnoringSafeArea(.all)
-            Text("Hello, world!")
-                .font(Font.custom("Pacifico-Regular", size: 40))
-                .bold()
-                .foregroundColor(.white)
+            VStack {
+                Image("eunsol")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Sasha")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .font(.system(size: 25))
+                    .foregroundColor(.white)
+            }
         }
     }
 }
